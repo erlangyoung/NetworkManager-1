NetworkManager/NetworkManager

commit 27bc2cb22a6bdd3dc0c1ad2a67464939dcf53049

device: Ensure P2P device is unavailable without wpa_supplicant
Put the device into UNAVAILABLE state when the corresponding WPA
supplicant management interface is unset. This is important to
explicitly clear any pending state changes that are not permissible when
there is no management interface.

src/devices/wifi/nm-device-wifi-p2p.c
@@ -945,6 +945,10 @@ supplicant_interfaces_release (NMDeviceWifiP2P *self, gboolean set_is_waiting)
 
 	supplicant_group_interface_release (self);
 
+	nm_device_state_changed (NM_DEVICE (self),
+	                         NM_DEVICE_STATE_UNAVAILABLE,
+	                         NM_DEVICE_STATE_REASON_SUPPLICANT_FAILED);
+
 	if (set_is_waiting)
 		_set_is_waiting_for_supplicant (self, TRUE);
 }

erlangyoung/NetworkManager-1

commit ea3501b7cafaf876aef653553e7e47ef32c7241f

wifi-p2p: fix failed assertion when releasing supplicant interface
supplicant_interfaces_release() can be called during a state change
(for example by device_state_changed()) and so it can't trigger
another state change. Move the state change to callers, only when
actually needed.

Fixes the following failed assertion:

 <debug> device[0x11dfec0] (p2p-dev-wlp4s0): P2P: Releasing WPA supplicant interface.
 <debug> supplicant: setting WFD IEs for P2P operation
 (../src/devices/nm-device.c:14769):_set_state_full: runtime check failed: (priv->in_state_changed == FALSE)
 <info>  device (p2p-dev-wlp4s0): state change: unmanaged -> unavailable (reason 'supplicant-failed', sys-iface-state: 'external')
 <debug> device[0x11dfec0] (p2p-dev-wlp4s0): add_pending_action (1): 'waiting-for-supplicant'

Fixes: 27bc2cb22a6bdd3dc0c1ad2a67464939dcf53049

src/devices/wifi/nm-device-wifi-p2p.c
@@ -710,6 +710,9 @@ supplicant_iface_state_cb (NMSupplicantInterface *iface,
 		                                   NM_DEVICE_STATE_REASON_SUPPLICANT_FAILED);
 
 		supplicant_interfaces_release (self, TRUE);
+		nm_device_state_changed (NM_DEVICE (self),
+		                         NM_DEVICE_STATE_UNAVAILABLE,
+		                         NM_DEVICE_STATE_REASON_SUPPLICANT_FAILED);
 		break;
 	default:
 		break;
@@ -941,10 +944,6 @@ supplicant_interfaces_release (NMDeviceWifiP2P *self, gboolean set_is_waiting)
 
 	supplicant_group_interface_release (self);
 
-	nm_device_state_changed (NM_DEVICE (self),
-	                         NM_DEVICE_STATE_UNAVAILABLE,
-	                         NM_DEVICE_STATE_REASON_SUPPLICANT_FAILED);
-
 	if (set_is_waiting)
 		_set_is_waiting_for_supplicant (self, TRUE);
 }
@@ -1115,8 +1114,12 @@ nm_device_wifi_p2p_set_mgmt_iface (NMDeviceWifiP2P *self,
 
 	supplicant_interfaces_release (self, FALSE);
 
-	if (!iface)
+	if (!iface) {
+		nm_device_state_changed (NM_DEVICE (self),
+		                         NM_DEVICE_STATE_UNAVAILABLE,
+		                         NM_DEVICE_STATE_REASON_SUPPLICANT_FAILED);
 		goto done;
+	}
 
 	_LOGD (LOGD_DEVICE | LOGD_WIFI, "P2P: WPA supplicant management interface changed to %s.", nm_supplicant_interface_get_object_path (iface));
 